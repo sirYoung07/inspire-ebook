26c69dda33701e323ab1d5804c84e3c7512b0286
unknown <onadiranbayonle358@gmail.com>
2023-03-20 20:33:30 +0000

    password resets

---
+++ app/Notifications/Notification/PasswordResetNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications\Notification;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordResetNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    protected $token;
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->greeting('Hello  ' . $notifiable->first_name)
+                    ->line('A password reset was requested using this email. If this was you, use the token below to reset your password')
+                    ->line($this->token)
+                    ->line('If you did not make this request, you need not do anything.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
